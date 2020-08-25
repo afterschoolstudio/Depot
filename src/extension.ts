8d3548b10429b433fc10be68dcfdb5de606d5e05
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2020-08-24 22:14:50 -0400

    gets initial sample working with reading in basic json and building a dynamic html layout from that

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// import { CantataTerrainEditorProvider } from './cantataTerrainEditor';
+import { CantataInteractableEditorProvider } from './cantataInteractableEditor';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	/*
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -21,6 +25,10 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	*/
+
+	// context.subscriptions.push(CantataTerrainEditorProvider.register(context));
+	context.subscriptions.push(CantataInteractableEditorProvider.register(context));
 }
 
 // this method is called when your extension is deactivated
