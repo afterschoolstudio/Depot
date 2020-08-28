1367e996acbb4b6e10bd92de0db528d212e30472
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2020-08-27 20:34:45 -0400

    got fields updating the main json object, could probably clean up the field declaration but it's working!

--- src/svelte/Field.svelte
+++ src/svelte/Field.svelte
@@ -1,5 +1,25 @@
-<!-- <script>
+<script>
+    import { createEventDispatcher } from 'svelte';
 
+    export let json;
+    export let key;
+    // $: value = json[key];
+    let value = json[key];
+    const dispatch = createEventDispatcher();
+    function fieldUpdated() {
+        dispatch('message', {
+            "key": key,
+            "value": value
+        });
+    }
+    // let value = interactableData[key];
+    // function updateValue() {
+    //     interactableData.update(d => d[key] = value)
+    // }
 </script>
 
-<input bind:value={interactableJSON.name} on:input={() => updateData("name")}> -->
+<label>
+    key: {key} <br>
+    <input bind:value={value} on:input={fieldUpdated}>
+    <!-- <input bind:value={value} > -->
+</label>
--- src/svelte/InteractableData.svelte
+++ src/svelte/InteractableData.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { onMount, setContext } from 'svelte';
+    // import { interactableData } from './dataStores.js';
     import Field from './Field.svelte';
+    // import { onDestroy } from 'svelte';
 
     // const vscode = acquireVsCodeApi();
     function handleClick() {
@@ -17,7 +18,6 @@
 				const text = message.text;
 
                 // Update our webview's content
-                // vscode.window.showInformationMessage("recieved update");
 				updateContent(text);
 
 				// Then persist state information.
@@ -31,15 +31,39 @@
     function updateData(key) {
         vscode.postMessage({
             type: 'updateDocumentFromInput',
-            update: {"key" : key, "value" : interactableJSON[key]}
+            update: {"key" : key, "value" : InteractableJSON[key]}
 		});
     }
 
-    let interactableJSON = {};
+    function handleMessage(event) {
+        console.log("filed update handle");
+        // console.log(event.detail.key);
+        //could switch here for data objects to pass in the json as well
+        InteractableJSON[event.detail.key] = event.detail.value;
+        updateData(event.detail.key);
+    }
+
+    let InteractableJSON = {};
+    // let interactableName;
+    // const unsubscribe = interactableData.subscribe(value => {
+    //     interactableName = value.name;
+    //     console.log("data updated from field update, new data:");
+    //     console.log(value);
+    //     vscode.postMessage({
+    //         type: 'updateDocumentFromInput',
+    //         update: value
+    //     });
+	// });
+
+	// onDestroy(unsubscribe);
+
+
+
 	function updateContent(/** @type {string} */ text) {
 		try {
-            interactableJSON = JSON.parse(text);
-            console.log(interactableJSON);
+            console.log(JSON.parse(text));
+            // interactableData.update(n => n = JSON.parse(text))
+            InteractableJSON = JSON.parse(text);
             // vscode.window.showInformationMessage(interactableJSON);
 		} catch {
             // vscode.window.showErrorMessage("json read issue");
@@ -55,7 +79,7 @@
 		updateContent(state.text);
     }
     
-    setContext("data", interactableJSON);
+    // setContext("data", interactableJSON);
 
     // setContext("data", {
     //     getData: () => data
@@ -64,11 +88,12 @@
 </script>
 
 <svelte:window on:message={windowMessage}/>
-<h1>{interactableJSON.name}</h1>
+<h1>{InteractableJSON.name}</h1>
 <!-- <button on:click={handleClick}>
     {interactableJSON.name}
 </button> -->
-<!-- <Field key={"name"} description={"something"}> -->
-<input bind:value={interactableJSON.name} on:input={() => updateData("name")}>
-<input bind:value={interactableJSON.another_value} on:input={() => updateData("another_value")}>
-<!-- <svelte:window on:={handleKeydown}/> -->
+<Field json={InteractableJSON} key={"name"} on:message={handleMessage}/>
+<br>
+<Field json={InteractableJSON} key={"another_value"} on:message={handleMessage}/>
+<!-- <input bind:value={interactableJSON.name} on:input={() => updateData("name")}>
+<input bind:value={interactableJSON.another_value} on:input={() => updateData("another_value")}> -->
