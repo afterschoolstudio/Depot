bc9adc0a077bc3bebabfe017f24da5ce0e56f970
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2020-09-22 00:17:34 -0400

    roughing in column field management for depot table

--- src/svelte/DataTypes/DepotTable.svelte
+++ src/svelte/DataTypes/DepotTable.svelte
@@ -1,38 +1,77 @@
 <script>
 import TextField from "../Fields/TextField.svelte";
+import { v4 as uuidv4 } from 'uuid';
+import { createEventDispatcher } from 'svelte';
+import { table } from "console";
 export let data;
 
-let showColumnEditor = false;
-let fieldEdit = "";
-function toggleColumnEditor(fieldType, visible) {
-    fieldEdit = fieldType;
-    showColumnEditor = visible;
+const dispatch = createEventDispatcher();
+function columnsUpdated() {
+    dispatch('message', {
+        "type" : "update"
+    });
 }
-function saveColumn(value) {
-    showColumnEditor = false;
+
+let openEditor = "";
+function addColumn(columnType) {
+    let uid = uuidv4();
+    switch (columnType) {
+        case "int":
+            data.columns.push({
+                "uid" : uid,
+                "typeStr": "int",
+                "name": "newInt"
+            });
+            break;
+        case "bool":
+            data.columns.push({
+                "uid" : uid,
+                "typeStr": "bool",
+                "name": "newBool"
+            });
+            break;
+        default:
+            break;
+    }
+    columnsUpdated();
+    openEditor = uid;
 }
+
+function closeEditor(){openEditor = "";}
+
 </script>
 
 <div>
     Name: <TextField bind:data={data.name} on:message/>
     <br>
 </div>
-<button on:click={() => toggleColumnEditor("int", true)}>Add Int Column</button>
-<button on:click={() => toggleColumnEditor("bool", true)}>Add Int Column</button>
+<button on:click={() => addColumn("int")}>Add Int</button>
+<button on:click={() => addColumn("bool")}>Add Bool</button>
 <br>
 
-{#if showColumnEditor}
-<p>{fieldEdit}</p>
-<button on:click={saveColumn}>Save Column</button>
-{/if}
-<table>
+
+
 {#if data.columns.length === 0}
     <p>No columns</p>
 {:else}
-    {#each data.columns as column}
-        <!-- <button on:click={focusSheet(data.sheets.indexOf(sheet))}>{sheet.name}</button> -->
-    {/each}
+    <table>
+    <tr>
+        <th>UID</th>
+        {#each data.columns as column}
+            <th><a href={"#"} on:click={()=> openEditor = column.uid}>{column.name}</a></th>
+        {/each}
+    </tr>
+    {#if openEditor !== ""}
+        <pre>{JSON.stringify(data.columns.find(o => o.uid === openEditor),null,2)}</pre>
+        <button on:click={closeEditor}>Close</button>
+        <button on:click={closeEditor}>Delete</button>
+    {/if}
+    <tr>
+        <td>UID</td>
+        <td>test</td>
+        <td>test</td>
+    </tr>
+    </table>
 {/if}
-</table>
 <pre>{JSON.stringify({data},null,2)}</pre>
 <p>----------------------------------------</p>
