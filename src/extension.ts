4fa369ae8e2dafb605c70b01bd0830d2f136d025
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2020-09-24 12:17:43 -0400

    adds in depot settings and adds in command to create a new depot file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { window } from 'vscode';
 import { posix } from 'path';
 // import { CantataTerrainEditorProvider } from './cantataTerrainEditor';
 // import { CantataInteractableEditorProvider } from './cantataInteractableEditor';
@@ -12,13 +13,37 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const command = 'depot.newDepotFile';
 
-	const commandHandler = (name: string = 'world') => {
-		const folderUri = vscode.workspace.workspaceFolders[0].uri;
-		//set these in depot settings config - default file name, default template
-		const fileUri = folderUri.with({ path: posix.join(folderUri.path, 'depot.dpo') });
-		const writeStr = '{ "sheets": []}';
-		const writeData = Buffer.from(writeStr, 'utf8');
-		vscode.workspace.fs.writeFile(fileUri,writeData);
+	const commandHandler = async () => {
+		let defFile = vscode.workspace.getConfiguration('depot').get('defaults.newFileName') + "";
+		const result = await window.showInputBox({
+			value: defFile,
+			valueSelection: [0, 0],
+			placeHolder: 'Enter the name of the new Depot file, including the extension'
+			// validateInput: text => {
+			// 	window.showInformationMessage(`Validating: ${text}`);
+			// 	return text === '123' ? 'Not 123!' : null;
+			// }
+		});
+
+		if(result !== undefined)
+		{
+			// @ts-ignore
+			const folderUri = vscode.workspace.workspaceFolders[0].uri;
+			//set these in depot settings config - default file name, default template
+			// @ts-ignore
+			const fileUri = folderUri.with({ path: posix.join(folderUri.path, result) });
+			const writeStr = '{ "sheets": []}';
+			const writeData = Buffer.from(writeStr, 'utf8');
+			vscode.workspace.fs.writeFile(fileUri,writeData);
+			// var openUri = fileUri.with({ scheme: 'file' });
+			// vscode.workspace.openTextDocument(openUri).then(doc => {
+			// 	vscode.window.createWebviewPanel(
+			// 		'cantata-tools.data',
+			// 		result,
+			// 		vscode.ViewColumn.One
+			// 	);
+			// });
+		}
 	};
 
 	context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
